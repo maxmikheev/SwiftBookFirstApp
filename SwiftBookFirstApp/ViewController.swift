d649517ae8207a03183cbbf84bd1363cd7f7c409
Maksim Mikheev <MMikheev@yotateam.com>
2021-05-18 23:32:19 +0300

    Initial Commit

---
+++ SwiftBookFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookFirstApp
+//
+//  Created by Максим Михеев on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
